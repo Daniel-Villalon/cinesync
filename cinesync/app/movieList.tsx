d5b28165b1082df3270efa6286f4c0ceaeb69dd8
EdmundXu26 <edmund.l.xu@gmail.com>
2025-05-12 16:52:22 -0700

    yuh

--- cinesync/app/movieList.tsx
+++ cinesync/app/movieList.tsx
@@ -625,7 +625,7 @@ const MovieList: React.FC<Props> = ({ groupId, initialType = 'watchlist' }) => {
                   // Watched view with ratings
                   <View style={styles.ratingContainer}>
                     <View style={styles.ratingSelectorContainer}>
-                      <Text style={styles.ratingLabel}>Your Rating:</Text>
+                      <Text style={styles.ratingLabel}>Group Rating:</Text>
                       {renderRatingSelector(item.imdbID, item.userRating)}
                     </View>
                     <TouchableOpacity
